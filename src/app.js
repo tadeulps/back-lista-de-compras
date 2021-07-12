5fffdaa6502551372e66d2d8a1b2139dde6e946b
Tadeu Juliace <tadeslopes@gmail.com>
2021-07-12 15:28:33 -0300

    Inicio da rota items

--- src/app.js
+++ src/app.js
@@ -7,4 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/items",async (req,res)=>{
+    try{
+    const {items}=req.body;
+    await connection.query(`INSERT INTO compras (items) 
+    VALUES ($1)`,[items]);
+    res.sendStatus(201)
+    }catch(error){
+    console.log(error)
+    res.sendStatus(500)
+    }
+});
+
+
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
 app.listen(4000, () => {
-  console.log(`Server is listening on port ${port}.`);
+  console.log(`Server is listening on port 4000.`);
 });
